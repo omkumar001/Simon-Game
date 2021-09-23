015d57ab111ee62c5384a288ba63605474a4abb2
OmThakur92 <omkumarbhu@gmail.com>
2021-09-23 19:59:25 +0530

    Updated the game.js and README.md files

---
+++ README.md
@@ -0,0 +1,8 @@
+# Simon-Game
+
+A fun game made using jQuery , HTML and CSS. Makes use of the event listener , DOM concepts and intermediate JS techniques.
+
+How to play :
+
+- Press any key to start .
+- Player has to remember the sequence of the tabs pressed and if he pression the wrong tab then the game ends .
--- game.js
+++ game.js
@@ -1,31 +1,79 @@
+
 var buttonColours = ["red", "blue", "green", "yellow"];
 
 var gamePattern = [];
-
-// $(".btn").click(function () {
-//   var randomNumber = Math.random() * 10;
-//   randomNumber = Math.floor(randomNumber);
-//   randomNumber = randomNumber % 4;
-
-//   var randomChosenColour = buttonColours[randomNumber];
-//   gamePattern.push(randomChosenColour);
-
-//   $("#" + randomChosenColour)
-//     .fadeIn(100)
-//     .fadeOut(100)
-//     .fadeIn(100);
-
-//   var butt = new Audio("sounds/" + randomChosenColour + ".mp3");
-//   butt.play();
-// });
-
-//New Section
-
 var userClickedPattern = [];
 
-$(".btn").click(function () {
-  var userChosenColour = $(this).attr("id");
+var started = false;
+var level = 0;
 
-  userClickedPattern.push(userChosenColour);
-  console.log(userClickedPattern);
+$(document).keypress(function() {
+  if (!started) {
+    $("#level-title").text("Level " + level);
+    nextSequence();
+    started = true;
+  }
 });
+
+$(".btn").click(function() {
+
+  var userChosenColour = $(this).attr("id");
+  userClickedPattern.push(userChosenColour);
+
+  playSound(userChosenColour);
+  animatePress(userChosenColour);
+
+  checkAnswer(userClickedPattern.length-1);
+});
+
+function checkAnswer(currentLevel) {
+
+    if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
+      if (userClickedPattern.length === gamePattern.length){
+        setTimeout(function () {
+          nextSequence();
+        }, 1000);
+      }
+    } else {
+      playSound("wrong");
+      $("body").addClass("game-over");
+      $("#level-title").text("Game Over, Press Any Key to Restart");
+
+      setTimeout(function () {
+        $("body").removeClass("game-over");
+      }, 200);
+
+      startOver();
+    }
+}
+
+
+function nextSequence() {
+  userClickedPattern = [];
+  level++;
+  $("#level-title").text("Level " + level);
+  var randomNumber = Math.floor(Math.random() * 4);
+  var randomChosenColour = buttonColours[randomNumber];
+  gamePattern.push(randomChosenColour);
+
+  $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
+  playSound(randomChosenColour);
+}
+
+function animatePress(currentColor) {
+  $("#" + currentColor).addClass("pressed");
+  setTimeout(function () {
+    $("#" + currentColor).removeClass("pressed");
+  }, 100);
+}
+
+function playSound(name) {
+  var audio = new Audio("sounds/" + name + ".mp3");
+  audio.play();
+}
+
+function startOver() {
+  level = 0;
+  gamePattern = [];
+  started = false;
+}
